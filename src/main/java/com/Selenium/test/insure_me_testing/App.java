f85f9deb1223fe11c42c4c32e76760962054776d
MadhusGitHub <73559597+MadhusGitHub@users.noreply.github.com>
2025-03-03 13:45:55 -0500

    Update App.java

--- src/main/java/com/Selenium/test/insure_me_testing/App.java
+++ src/main/java/com/Selenium/test/insure_me_testing/App.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
     	
     	
 //adding headless browser options to sync to Ubuntu
